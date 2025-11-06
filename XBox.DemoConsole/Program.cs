c55626116e0a1983473cff8fb73184e9def77b45
Luis Goll <luislhg@hotmail.com>
2025-11-06 12:26:25 -0300

    Improved Console Demo

--- XBox.DemoConsole/Program.cs
+++ XBox.DemoConsole/Program.cs
@@ -2,6 +2,7 @@
 {
     public class Program
     {
+        private static string lastState = "";
         static void Main(string[] args)
         {
             // Set up the controller watcher to monitor connection events
@@ -9,14 +10,29 @@
             xbcw.ControllerConnected += OnControllerConnected;
             xbcw.ControllerDisconnected += OnControllerDisconnected;
 
+            // Give some time for initial connections to be detected
+            Thread.Sleep(1000);
+
             // Main loop to read controller states, exit on key press
             Console.WriteLine("Press any key to exit.");
             while (!Console.KeyAvailable)
             {
-                Thread.Sleep(1000);
+                Thread.Sleep(200);
                 foreach (var c in XBoxController.GetConnectedControllers())
                 {
-                    Console.WriteLine("Controller " + c.PlayerIndex.ToString() + " Thumb Left X = " + c.ThumbLeftX.ToString() + ", Y = " + c.ThumbLeftY.ToString() + ", A = " + c.ButtonAPressed.ToString());
+                    var state = "Controller " + c.PlayerIndex.ToString() +
+                        ": Thumb Left (x=" + c.ThumbLeftX.ToString("N0").PadRight(3) + " y=" + c.ThumbLeftY.ToString("N0").PadRight(3) + ")" +
+                        " | Thumb Right (x=" + c.ThumbRightX.ToString("N0").PadRight(3) + "y=" + c.ThumbRightY.ToString("N0").PadRight(3) + ")" +
+                        " | X = " + c.ButtonXPressed.ToString().PadRight(5) +
+                        " | A = " + c.ButtonAPressed.ToString().PadRight(5) +
+                        " | Y = " + c.ButtonYPressed.ToString().PadRight(5) +
+                        " | B = " + c.ButtonBPressed.ToString().PadRight(5);
+
+                    if (state != lastState)
+                    {
+                        lastState = state;
+                        Console.WriteLine(state);
+                    }
                 }
             }
         }
